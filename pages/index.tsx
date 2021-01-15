83d1568aff3a266f3be29b30b171da8bf84e44a4
Daniel Wallace <41707979+da-wallace@users.noreply.github.com>
2021-01-15 17:53:07 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,29 +45,27 @@ const IndexPage: React.FC<IIndexPageProps> = ({ currentUser }) => {
             <div className="text-sm font-semibold mt-2">{currentUser?.username}</div>
           </div>
           <div className="flex flex-col mt-8">
-            <div className="flex flex-col mt-8">
-              <div className="flex flex-row items-center justify-between text-xs">
-                <span className="font-bold">Active</span>
-                <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
-                  {activeUsers.length}
-                </span>
-              </div>
-              <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-                {activeUsers.map((user) => (
-                  <User user={user} key={user.id} onClick={() => null} />
-                ))}
-              </div>
-              <div className="flex flex-row items-center justify-between text-xs mt-6">
-                <span className="font-bold">Inactive</span>
-                <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
-                  {inactiveUsers.length}
-                </span>
-              </div>
-              <div className="flex flex-col space-y-1 mt-4 -mx-2">
-                {inactiveUsers.map((user) => (
-                  <User user={user} key={user.id} onClick={() => null} />
-                ))}
-              </div>
+            <div className="flex flex-row items-center justify-between text-xs">
+              <span className="font-bold">Active</span>
+              <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
+                {activeUsers.length}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
+              {activeUsers.map((user) => (
+                <User user={user} key={user.id} onClick={() => null} />
+              ))}
+            </div>
+            <div className="flex flex-row items-center justify-between text-xs mt-6">
+              <span className="font-bold">Inactive</span>
+              <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
+                {inactiveUsers.length}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-1 mt-4 -mx-2">
+              {inactiveUsers.map((user) => (
+                <User user={user} key={user.id} onClick={() => null} />
+              ))}
             </div>
           </div>
         </div>
